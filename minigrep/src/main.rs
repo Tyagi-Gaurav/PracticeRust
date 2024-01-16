a8011d22b320b23a5f856962f3f5b520cbf7ab2b
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2024-01-16 17:46:29 +0000

    Minigrep

---
+++ minigrep/src/main.rs
@@ -0,0 +1,16 @@
+use std::{process, env};
+use minigrep::{Config, run};
+
+fn main() {
+    let args: Vec<String> = env::args().collect(); //Collect arguments as iterator
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem processing arguments {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = run(config) { //Using let instead of unwrap as run doesn't return a value that we want to unwrap
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+}
